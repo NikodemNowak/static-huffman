7f465360538cc584e0ae8f1b733e9fd609e0b061
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2025-05-03 18:25:19 +0200

    Add some comments to better understanding the code

--- src/main/java/org/zespol/huffman/HuffmanEncoder.java
+++ src/main/java/org/zespol/huffman/HuffmanEncoder.java
@@ -4,18 +4,16 @@ import java.util.*;
 
 public class HuffmanEncoder {
     private Map<Character, String> encodingMap;
+    private Map<Character, Integer> frequencyMap;
+    private HuffmanNode root;
 
     public Map<Character, Integer> getFrequencyMap() {
         return frequencyMap;
     }
 
-    private Map<Character, Integer> frequencyMap;
-
     public HuffmanNode getRoot() {
         return root;
     }
-
-    private HuffmanNode root;
 
     public HuffmanEncoder() {
         this.encodingMap = new HashMap<>();
@@ -39,7 +37,12 @@ public class HuffmanEncoder {
 
     private void createFrequencyMap(String message) {
         Map<Character, Integer> frequencyMap = new HashMap<>();
+        // Iterujemy po każdym znaku w wiadomości i aktualizujemy jego częstotliwość
         for (char c : message.toCharArray()) {
+            // Używamy metody merge:
+            // c to klucz,
+            // 1 to wartość która będzie ustawiona dla nowego znaku lub połączona z istniejącą,
+            // Integer::sum to funkcja łącząca, która mówiem, że jak taki znak już istnieje to dodaj do jego wartości 1
             frequencyMap.merge(c, 1, Integer::sum);
         }
         this.frequencyMap = frequencyMap;
@@ -48,29 +51,45 @@ public class HuffmanEncoder {
     private void buildTree(Map<Character, Integer> frequencyMap) {
         PriorityQueue<HuffmanNode> queue = new PriorityQueue<>();
 
+        // frequencyMap.entrySet() - zwraca zbiór par klucz-wartość
+        // entry - to pojedyncza para klucz-wartość
         for (Map.Entry<Character, Integer> entry : frequencyMap.entrySet()) {
+            // Tworzymy nowy węzeł Huffmana z kluczem i wartością
             HuffmanNode node = new HuffmanNode(entry.getKey(), entry.getValue());
+            // Dodajemy węzeł do kolejki priorytetowej, priorytetowa kolejka sortuje węzły na podstawie ich częstotliwości, czyli wartości
             queue.offer(node);
         }
 
         while (queue.size() > 1) {
+            // Pobieramy dwa węzły o najmniejszej częstotliwości
             HuffmanNode left = queue.poll();
             HuffmanNode right = queue.poll();
+
+            // Tworzymy nowy węzeł, który będzie rodzicem tych dwóch węzłów
+            // Jego znak to '\0' (brak znaku), a częstotliwość to suma częstotliwości obu węzłów
             HuffmanNode parent = new HuffmanNode('\0', left.getFrequency() + right.getFrequency());
+            // Ustawiamy lewego i prawego syna dla rodzica
             parent.setLeft(left);
             parent.setRight(right);
+
+            // Dodajemy rodzica do kolejki
             queue.offer(parent);
         }
 
+        // Na końcu w kolejce zostaje tylko jeden węzeł, który jest korzeniem drzewa Huffmana
         root = queue.poll();
     }
 
+    // Rekurencyjna metoda do tworzenia mapy kodowania, czyli przypisania kodów do symboli np A -> 00, B -> 01 itd.
+    // Zaczynamy od korzenia i pustego kodu
     private void createEncodingMap(HuffmanNode node, String code) {
         if (node == null) {
             return;
         }
 
+        // Jeśli węzeł jest liściem - brak left i right
         if (node.isLeaf()) {
+            // Przypisujemy kod do symbolu
             encodingMap.put(node.getSymbol(), code);
             return;
         }
